9786739d642211cc7fdf945e5f094208b9b06d90
Mateusz Korczynski <mateusz.korczynski@capgemini.com>
2019-02-12 10:53:46 +0100

    resolves #1 Add Service Worker / Offline Mode

--- projects/tech-talk-se/src/app/app.module.ts
+++ projects/tech-talk-se/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { EventModule } from './event/event.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { EventModule } from './event/event.module';
   imports: [
     BrowserModule,
     CoreModule,
-    EventModule
+    EventModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
